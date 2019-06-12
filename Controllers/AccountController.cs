2b2323bcf82269ddfccdecd113fe24b841df8105
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-11 17:04:40 -0700

    feat(AccountController): add WIP `register` route and references

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 using dotnet_core_identity_sandbox.Areas.Identity.Data;
 using dotnet_core_identity_sandbox.Models;
@@ -16,37 +19,55 @@ namespace dotnet_core_identity_sandbox.Controllers
     {
         private readonly UserManager<UserEntity> _userManager;
         private readonly SignInManager<UserEntity> _signInManager;
+        private readonly ILogger<AccountController> _logger;
         private readonly IPasswordHasher<UserEntity> _passwordHasher;
+        private readonly IEmailSender _emailSender;
 
         public AccountController(
             UserManager<UserEntity> userManager,
             SignInManager<UserEntity> signInManager,
-            IPasswordHasher<UserEntity> passwordHasher)
+            ILogger<AccountController> logger,
+            IPasswordHasher<UserEntity> passwordHasher,
+            IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
             _passwordHasher = passwordHasher;
+            _emailSender = emailSender;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Create([FromBody] Login credentials)
-        {
-            if (!ModelState.IsValid)
+        public async Task<IActionResult> Register([FromBody] Login credentials)
+        {  
+            if (ModelState.IsValid)
             {
-                return BadRequest(ModelState.Values.SelectMany(v => v.Errors).Select(modelError => modelError.ErrorMessage).ToList());
+                var user = new UserEntity { UserName = credentials.Email, Email = credentials.Email };
+                var result =  await _userManager.CreateAsync(user, credentials.Password);
+                
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(credentials.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    return Ok();
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
             
-            var user = new UserEntity { UserName = credentials.Email, Email = credentials.Email };
-            var result =  await _userManager.CreateAsync(user, credentials.Password);
-            
-            if (!result.Succeeded)
-            {
-                return BadRequest(result.Errors.Select(x => x.Description).ToList());
-            }
-            
-            await _signInManager.SignInAsync(user, false);
-            
-            return Ok();
+            return BadRequest(ModelState.Values.SelectMany(v => v.Errors).Select(modelError => modelError.ErrorMessage).ToList());
         }
 
         [HttpPost("login")]
@@ -75,7 +96,6 @@ namespace dotnet_core_identity_sandbox.Controllers
                 }
             }
 
-            // If we got this far, something failed, redisplay form
             return BadRequest("Invalid login attempt.");
         }
     }
