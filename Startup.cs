2934f26f696cdb1dec7e5fdf325642cffffadf90
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-12 15:23:39 -0700

    feat(Startup): add and call `CreateUserRoles`

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using dotnet_core_identity_sandbox.Areas.Identity.Data;
+
 using WebPWrecover.Services;
 
 namespace dotnet_core_identity_sandbox
@@ -36,13 +38,15 @@ namespace dotnet_core_identity_sandbox
             });
 
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped<RoleManager<IdentityRole>>();
             services.Configure<AuthMessageSenderOptions>(Configuration);
+			services.AddAuthentication().AddJwtBearer();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -57,10 +61,51 @@ namespace dotnet_core_identity_sandbox
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
             app.UseAuthentication();
+            CreateUserRoles(serviceProvider).Wait();
 
             app.UseMvc();
         }
+
+		// TODO: Break out functionality to indicate superuser creation side effect.
+		private async Task CreateUserRoles(IServiceProvider serviceProvider)
+		{
+			//initializing custom roles 
+			var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+			var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+			string[] roleNames = { "Administrator", "Consumer" };
+			IdentityResult roleResult;
+
+			foreach (var roleName in roleNames)
+			{
+				var roleExist = await RoleManager.RoleExistsAsync(roleName);
+				if (!roleExist)
+				{
+					//create the roles and seed them to the database: Question 1
+					roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+				}
+			}
+
+			//Here you could create a super user who will maintain the web app
+			var poweruser = new ApplicationUser
+			{
+				UserName = Configuration["AppSettings:UserName"],
+				Email = Configuration["AppSettings:UserEmail"],
+			};
+
+			//Ensure you have these values in your appsettings.json file
+			string userPWD = Configuration["AppSettings:UserPassword"];
+			var _user = await UserManager.FindByEmailAsync(Configuration["AppSettings:AdminUserEmail"]);
+
+			if (_user == null)
+			{
+				var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+				if (createPowerUser.Succeeded)
+				{
+					//here we tie the new user to the role
+					await UserManager.AddToRoleAsync(poweruser, "Administrator");
+				}
+			}
+		}
     }
 }
