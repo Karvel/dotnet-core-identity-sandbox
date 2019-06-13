c697e89fef57077ce46c927e69eb3c2747d92d1b
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-13 14:55:22 -0700

    feat(Models): add `Register` class

--- Models/Register.cs
+++ Models/Register.cs
@@ -2,12 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace dotnet_core_identity_sandbox.Models
 {
-    public class Login
+    public class Register
     {
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
             [Required]
             [EmailAddress]
             public string Email { get; set; }
-
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
