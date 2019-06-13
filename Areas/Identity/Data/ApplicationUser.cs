f5464977ec1405cec7122bf8214d47838ac7243c
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-13 15:10:45 -0700

    feat(ApplicationUser): add custom fields to `User` entity

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -9,5 +9,9 @@ namespace dotnet_core_identity_sandbox.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        public string FirstName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
     }
 }
