f8885a5c4d59e1ce9a3eb371c11c90832a2f3d20
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-13 15:11:21 -0700

    chore(Migrations): add `AddCustomUserFields` migration

--- Migrations/20190613215004_AddCustomUserFields.Designer.cs
+++ Migrations/20190613215004_AddCustomUserFields.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotnet_core_identity_sandbox.Areas.Identity.Data;
 
 namespace dotnet_core_identity_sandbox.Migrations
 {
     [DbContext(typeof(IdentityDataContext))]
-    partial class IdentityDataContextModelSnapshot : ModelSnapshot
+    [Migration("20190613215004_AddCustomUserFields")]
+    partial class AddCustomUserFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
